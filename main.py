bc2b5ae761d74589c8d6a013200e1ac8c77ecfd1
Brian O'Sullivan <bosullivan@transfermate.com>
2020-08-07 14:37:06 +0100

    complete player movements

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ pygame.display.set_icon(pygame.image.load("images/icon.png"))
 playerImg = pygame.image.load("images/player.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
 
 def player(x, y):
@@ -21,20 +23,30 @@ def player(x, y):
 # Game Loop
 running = True
 while running:
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                pass
+                playerX_change = -0.3
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                pass
+                playerX_change = +0.3
             if event.key == pygame.K_UP or event.key == pygame.K_w:
-                pass
+                playerY_change = -0.3
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                pass
+                playerY_change = +0.3
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_w or event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                playerY_change = 0
+
+    playerX += playerX_change
+    playerY += playerY_change
     player(playerX, playerY)
     pygame.display.update()
 
